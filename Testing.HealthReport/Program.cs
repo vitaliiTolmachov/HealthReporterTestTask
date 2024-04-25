e7873ca5824291e9c994df69319b92bda58c3ce8
Vitalii Tolmachov <v.tolmachov@gmail.com>
2024-04-25 14:55:37 +0300

    Health report calculated for each date

--- Testing.HealthReport/Program.cs
+++ Testing.HealthReport/Program.cs
@@ -16,5 +16,33 @@ var healthData = new List<HealthDataItem>()
     new ("Service1", DateTimeOffset.Parse("2023-07-11 03:55:04 +03:00"), HealthStatus.Unhealthy),
     new ("Service1", DateTimeOffset.Parse("2023-07-11 04:15:04 +03:00"), HealthStatus.Healthy)
 };
-Console.WriteLine(dateProvider.OffsetNow);
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"Report generated date: {dateProvider.OffsetNow}");
+
+var endDate = dateProvider.OffsetNow;
+var startDate = endDate.AddDays(-14);
+
+for (var currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+{
+    var healthItemsForDate = healthData.Where(item => item.Date.Date == currentDate.Date).ToList();
+    
+    if (healthItemsForDate.Count == 0)
+    {
+        Console.WriteLine($"{currentDate.Date:yyyy-MM-dd} Unavailable");
+    }
+    else
+    {
+        var totalItems = healthItemsForDate.Count;
+        var healthyItems = healthItemsForDate.Count(item => item.Status == HealthStatus.Healthy);
+        var degradedItems = healthItemsForDate.Count(item => item.Status == HealthStatus.Degraded);
+        var unhealthyItems = healthItemsForDate.Count(item => item.Status == HealthStatus.Unhealthy);
+
+        var uptimePercent = (double) healthyItems / totalItems * 100;
+        var degradedPercent = (double) degradedItems / totalItems * 100;
+        var unhealthyPercent = (double) unhealthyItems / totalItems * 100;
+        
+        var serviceName = healthItemsForDate.First().Service;
+
+        Console.WriteLine(
+            $"{currentDate.Date:yyyy-MM-dd} {serviceName} {uptimePercent:F2}% {unhealthyPercent:F2}% {degradedPercent:F2}%");
+    }
+}
