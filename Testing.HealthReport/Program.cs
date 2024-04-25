29dce466c7ae409ebe91bcff201583a8daa19366
Vitalii Tolmachov <v.tolmachov@gmail.com>
2024-04-25 14:36:41 +0300

    Initial commit

---
+++ Testing.HealthReport/Program.cs
@@ -0,0 +1,20 @@
+﻿// Print health report for past 14 days for each particular day, use IDateTimeProvider to get current date
+// Format: {ServiceName} {Date} {Uptime} {UptimePercent} {UnhealthyPercent} {DegradedPercent}
+// Consider health data could be unavailable, for example monitoring started 1 day ago, in that case display Unavailable for periods preceding
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Testing.HealthReport;
+
+IDateTimeProvider dateProvider = new DateTimeProvider();
+var healthData = new List<HealthDataItem>()
+{
+    new ("Service1", DateTimeOffset.Parse("2023-07-01 05:50:34 +03:00"), HealthStatus.Healthy),
+    new ("Service1", DateTimeOffset.Parse("2023-07-02 05:50:34 +03:00"), HealthStatus.Unhealthy),
+    new ("Service1", DateTimeOffset.Parse("2023-07-09 05:50:34 +03:00"), HealthStatus.Healthy),
+    new ("Service1", DateTimeOffset.Parse("2023-07-10 03:50:34 +03:00"), HealthStatus.Degraded),
+    new ("Service1", DateTimeOffset.Parse("2023-07-10 03:55:04 +03:00"), HealthStatus.Healthy),
+    new ("Service1", DateTimeOffset.Parse("2023-07-11 03:55:04 +03:00"), HealthStatus.Unhealthy),
+    new ("Service1", DateTimeOffset.Parse("2023-07-11 04:15:04 +03:00"), HealthStatus.Healthy)
+};
+Console.WriteLine(dateProvider.OffsetNow);
+Console.WriteLine("Hello, World!");
